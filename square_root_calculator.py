a8bfd7844c15d5393fb69f7686aaf45049bd451b
khushbu0152 <55647992+khushbu0152@users.noreply.github.com>
2024-11-14 02:09:33 +0530

    Update square_root_calculator.py

--- square_root_calculator.py
+++ square_root_calculator.py
@@ -2,5 +2,5 @@ import math
 
 def calculate_square_root(num):
     if num < 0:
-        raise ValueError("Input must be non-negative!!!")
+        raise ValueError("Input must be non-negative...")
     return math.sqrt(num)
